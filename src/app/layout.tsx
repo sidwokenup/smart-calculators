f32b33155af1593caea9300d39459fc67a35001d
sidwokenup_ <wakedupsid2023@gmail.com>
2026-04-29 04:59:36 +0530

    gtag added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import "./globals.css";
@@ -18,6 +19,24 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="scroll-smooth">
+      <head>
+        <Script
+          strategy="afterInteractive"
+          src={`https://www.googletagmanager.com/gtag/js?id=G-EBD71DKE32`}
+        />
+        <Script
+          id="google-analytics"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-EBD71DKE32');
+            `,
+          }}
+        />
+      </head>
       <body
         suppressHydrationWarning={true}
         className="antialiased min-h-screen bg-[#F9FAFB] text-[#111827] flex flex-col overflow-x-hidden"
